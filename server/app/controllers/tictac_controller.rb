24d5642fa82ea9a91fb84180e6fdff379ca37e15
Adolfo Builes <builes.adolfo@googlemail.com>
2011-05-16 18:49:23 -0500

    skeleton

--- server/app/controllers/tictac_controller.rb
+++ server/app/controllers/tictac_controller.rb
@@ -1,5 +1,9 @@
 class TictacController < ApplicationController
   def move
+    TicTac.move(params[:user_id], params[:table], params[:play])
   end
 
+  def check_game
+    TicTac.play(params[:user_id], params[:table])
+  end
 end
